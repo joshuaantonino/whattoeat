381b3e536d3a403c9e48ea6c28d3500f16daeb90
Joshua Antonino <joshuamempinantonino@gmail.com>
2023-01-09 20:29:41 +0800

    What to eat

--- app/src/main/java/com/example/whattoeat/Categories.java
+++ app/src/main/java/com/example/whattoeat/Categories.java
@@ -2,7 +2,12 @@ package com.example.whattoeat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -11,6 +16,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class Categories extends AppCompatActivity {
 
+    private Button btnglist,  btnrecipes, btnfridge;
+    private ImageButton Logout;
     GoogleSignInOptions gso;
     GoogleSignInClient gsc;
 
@@ -18,6 +25,42 @@ public class Categories extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+        //lOGOUT FUNCTION
+        Logout = findViewById(R.id.Logout);
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Categories.this, WhattoEat.class));
+            }
+        });
+
+        //GROCERY LIST FUNCTION
+        btnglist = findViewById(R.id.btnglist);
+        btnglist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Categories.this, GroceryList.class));
+            }
+        });
+
+        //RECIPES LIST FUNCTION
+        btnrecipes = findViewById(R.id.btnrecipes);
+        btnrecipes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Categories.this, AllRecipes.class));
+            }
+        });
+
+        //FRIDGE ITEMS LIST FUNCTION
+        btnfridge = findViewById(R.id.btnfridge);
+        btnfridge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Categories.this, Fridge.class));
+            }
+        });
 
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         gsc = GoogleSignIn.getClient(this, gso);
